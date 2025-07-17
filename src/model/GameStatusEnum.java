e3712f4bcaca85125cd22c61c7fec8ddc3245921
Jhonata Jansen <codejhonatajansen@gmail.com>
2025-07-17 10:23:12 -0300

    feito toda a logica da classe main

--- src/model/GameStatusEnum.java
+++ src/model/GameStatusEnum.java
@@ -1,7 +1,17 @@
 package model;
 
 public enum GameStatusEnum {
-    NOM_STARTED,
-    INCOMPLETE,
-    COMPLETE
+    NOM_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
